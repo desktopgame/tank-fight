b8c72d048550ef312d68115e8be2dad9a4542728
Koya <koya0306@outlook.jp>
2019-05-22 22:09:37 +0900

    setup glfw

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,7 +1,33 @@
-#include <iostream>
-#include <string>
+#include <GLFW/glfw3.h>
 
-int main(int argc, char *argv[]) {
-        std::cout << "hello, world" << std::endl;
+int main(void) {
+        GLFWwindow* window;
+
+        /* Initialize the library */
+        if (!glfwInit()) return -1;
+
+        /* Create a windowed mode window and its OpenGL context */
+        window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+        if (!window) {
+                glfwTerminate();
+                return -1;
+        }
+
+        /* Make the window's context current */
+        glfwMakeContextCurrent(window);
+
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(window)) {
+                /* Render here */
+                glClear(GL_COLOR_BUFFER_BIT);
+
+                /* Swap front and back buffers */
+                glfwSwapBuffers(window);
+
+                /* Poll for and process events */
+                glfwPollEvents();
+        }
+
+        glfwTerminate();
         return 0;
 }
