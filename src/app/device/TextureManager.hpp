27044eea834684fb0a4e91713c2a0ccca20c9c41
Koya <koya0306@outlook.jp>
2019-05-25 23:56:41 +0900

    add comment

--- src/app/device/TextureManager.hpp
+++ src/app/device/TextureManager.hpp
@@ -6,12 +6,33 @@
 #include <unordered_map>
 namespace mygame {
 class ITexture;
+/**
+ * TextureManager is manage a `ITexture`.
+ * in now, supported .bmp and .png
+ */
 class TextureManager {
        public:
         TextureManager();
+        /**
+         * load a .bmp file.
+         * @param path
+         */
         void loadBmp(const std::string& path);
+        /**
+         * load a .png file
+         * @param path
+         */
         void loadPng(const std::string& path);
+        /**
+         * return a texture.
+         * @param path
+         * @return
+         */
         std::shared_ptr<ITexture> getTexture(const std::string& path);
+        /**
+         * unload texture.
+         * @param path
+         */
         void unload(const std::string& path);
 
        private:
