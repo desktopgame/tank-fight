fe30a1d3de3a5e1c3dbf80275552b0c340d81b7d
Koya <koya0306@outlook.jp>
2019-05-25 23:53:09 +0900

    add comment

--- src/app/device/AudioManager.hpp
+++ src/app/device/AudioManager.hpp
@@ -7,13 +7,43 @@
 
 namespace mygame {
 class IPlayableAudio;
+/**
+ * AudioManager is manage a `IPlayableAudio`
+ * in now, supported a .wave only
+ */
 class AudioManager {
        public:
         AudioManager();
+        /**
+         * load a .wave file.
+         * @param path
+         */
         void loadWave(const std::string& path);
+        /**
+         * play audio.
+         * should be load file in before call.
+         * @param path
+         */
         void play(const std::string& path) const;
+
+        /**
+         * return a buffer of OpenAL.
+         * @param path
+         * @return
+         */
         GLuint getBuffer(const std::string& path) const;
+
+        /**
+         * return a source of OpenAL.
+         * @param path
+         * @return
+         */
         GLuint getSource(const std::string& path) const;
+
+        /**
+         * unload audio.
+         * @param path
+         */
         void unload(const std::string& path);
 
        private:
