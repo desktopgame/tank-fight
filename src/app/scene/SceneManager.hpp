0e284d3f81f2f55a6ad3d6d49f4fcd0924040a94
Koya <koya0306@outlook.jp>
2019-05-26 00:05:34 +0900

    add comment

--- src/app/scene/SceneManager.hpp
+++ src/app/scene/SceneManager.hpp
@@ -7,14 +7,42 @@
 
 namespace mygame {
 class IScene;
+/**
+ * SceneManager is manage a `IScene`.
+ * this class is can to transition of scene to scene.
+ */
 class SceneManager {
        public:
         SceneManager();
+        /**
+         * be scene mapping by name.
+         * @param name
+         * @param scene
+         */
         void put(const std::string& name, const std::shared_ptr<IScene>& scene);
+        /**
+         * remove a mapping.
+         * @param name
+         */
         void remove(const std::string& name);
+        /**
+         * set a current scene.
+         * @param name
+         */
         void bind(const std::string& name);
+        /**
+         * update a current scene.
+         * be transition if finished current scene.
+         */
         void update();
+        /**
+         * draw a current scene.
+         */
         void draw();
+        /**
+         * return a current scene.
+         * @return
+         */
         std::optional<std::shared_ptr<IScene> > getScene();
 
        private:
