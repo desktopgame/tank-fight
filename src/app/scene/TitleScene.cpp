b1005f8d3b2b8108406da8f1aac40920f60e24d6
Koya <koya0306@outlook.jp>
2019-05-26 14:46:52 +0900

    test Fbx lineup

--- src/app/scene/TitleScene.cpp
+++ src/app/scene/TitleScene.cpp
@@ -18,15 +18,20 @@ void TitleScene::show() {}
 void TitleScene::update() {}
 
 void TitleScene::draw() {
-        auto model = mModelManager->getModel("./assets/model/Gun1028.fbx");
+        auto model = mModelManager->getModel("./assets/model/Block.fbx");
         auto aabb = model->getAABB();
-        auto points = aabb.points();
-        glPushMatrix();
-        glScalef(0.02f, 0.02f, 0.02f);
-        model->draw();
-        glColor3f(1.0f, 0, 0);
-        aabb.drawFrame();
-        glPopMatrix();
+        auto size = aabb.getSize();
+        for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                        glPushMatrix();
+                        glTranslatef(size.x * i * 0.02f, 0, size.z * j * 0.02f);
+                        glScalef(0.02f, 0.02f, 0.02f);
+                        model->draw();
+                        glColor3f(1.0f, 0, 0);
+                        aabb.drawFrame();
+                        glPopMatrix();
+                }
+        }
 }
 
 std::string TitleScene::getNextScene() const { return "play"; }
