48e2dfb3386c1eeae10490d3ca88a81543619d61
Koya <koya0306@outlook.jp>
2019-05-29 10:37:27 +0900

    add comment

--- src/gel/model/Vector2.hpp
+++ src/gel/model/Vector2.hpp
@@ -1,13 +1,26 @@
 #ifndef GEL_MODEL_VECVector2OR2_HPP
 #define GEL_MODEL_VECVector2OR2_HPP
 namespace gel {
+/**
+ * Vector2 is two dimensional vector.
+ */
 struct Vector2 {
         Vector2();
         Vector2(float x, float y);
         float x;
         float y;
+        /**
+         * @return
+         */
         float length() const;
+        /**
+         * @return
+         */
         Vector2& normalize();
+        /**
+         * @param v
+         * @return
+         */
         float dot(const Vector2& v) const;
         Vector2 operator-() const;
         Vector2& operator*=(float scale);
--- src/gel/model/Vector3.hpp
+++ src/gel/model/Vector3.hpp
@@ -1,15 +1,32 @@
 #ifndef GEL_MODEL_VECTOR3_HPP
 #define GEL_MODEL_VECTOR3_HPP
 namespace gel {
+/**
+ * Vector3 is three dimensional vector.
+ */
 struct Vector3 {
         Vector3();
         Vector3(float x, float y, float z);
         float x;
         float y;
         float z;
+        /**
+         * @return
+         */
         float length() const;
+        /**
+         * @return
+         */
         Vector3& normalize();
+        /**
+         * @param v
+         * @return
+         */
         float dot(const Vector3& v) const;
+        /**
+         * @param v
+         * @return
+         */
         Vector3 cross(const Vector3& v) const;
         Vector3 operator-() const;
         Vector3& operator*=(float scale);
