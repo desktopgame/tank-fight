6907d1c0efc010d4976fdaf335d3df697e451ccf
Koya <koya0306@outlook.jp>
2019-06-06 15:13:13 +0900

    add left, top, right, bottom

--- src/gel/model/Rect.cpp
+++ src/gel/model/Rect.cpp
@@ -4,4 +4,8 @@ Rect::Rect(const Vector2& position, const Vector2& size)
     : position(position), size(size) {}
 
 Rect::Rect(float x, float y, float w, float h) : position(x, y), size(w, h) {}
+float Rect::left() const { return position.x; }
+float Rect::top() const { return position.y; }
+float Rect::right() const { return position.x + size.x; }
+float Rect::bottom() const { return position.y + size.y; }
 }  // namespace gel
--- src/gel/model/Rect.hpp
+++ src/gel/model/Rect.hpp
@@ -5,6 +5,10 @@ namespace gel {
 struct Rect {
         Vector2 position;
         Vector2 size;
+        float left() const;
+        float top() const;
+        float right() const;
+        float bottom() const;
         explicit Rect(const Vector2& position, const Vector2& size);
         explicit Rect(float x, float y, float w, float h);
 };
