99dcc104329995d713ffbdde4a82288668cb808e
Koya <koya0306@outlook.jp>
2019-06-10 10:41:40 +0900

    update ResultScene

--- src/game/scene/ResultScene.cpp
+++ src/game/scene/ResultScene.cpp
@@ -1,6 +1,18 @@
 #include "ResultScene.hpp"
-ResultScene::ResultScene(const std::shared_ptr<gel::TextureManager>& textureManager, PlayResult& playResult) : 
- textureManager(textureManager),playResult(playResult), finished(false),yourScoreTimer(2),isTimer(1),scoreTimer(1), phase(0),scoreNum(
+ResultScene::ResultScene(
+    const std::shared_ptr<gel::TextureManager>& textureManager,
+    PlayResult& playResult)
+    : textureManager(textureManager),
+      playResult(playResult),
+      finished(false),
+      yourScoreTimer(2),
+      isTimer(1),
+      scoreTimer(1),
+      returnTimer(1),
+      transTimer(0.5f),
+      transStart(false),
+      phase(0),
+      scoreNum(
           textureManager,
           std::array<std::string, 10>{"./assets/image/number/0_R50x50.png",
                                       "./assets/image/number/1_R50x50.png",
@@ -11,65 +23,103 @@ ResultScene::ResultScene(const std::shared_ptr<gel::TextureManager>& textureMana
                                       "./assets/image/number/6_R50x50.png",
                                       "./assets/image/number/7_R50x50.png",
                                       "./assets/image/number/8_R50x50.png",
-                                      "./assets/image/number/9_R50x50.png"}) {
-}
+                                      "./assets/image/number/9_R50x50.png"}),
+      alphaDuration(1) {}
 void ResultScene::show() {
-	yourScoreTimer.reset();
-	isTimer.reset();
-	scoreTimer.reset();
-	this->phase = 0;
+        yourScoreTimer.reset();
+        isTimer.reset();
+        scoreTimer.reset();
+        this->phase = 0;
+        this->transStart = false;
 }
 void ResultScene::update() {
-	if(phase == 0) {
-		yourScoreTimer.update();
-		if(yourScoreTimer.isElapsed()) {
-			this->phase = 1;
-		}
-	} else if(phase == 1) {
-		isTimer.update();
-		if(isTimer.isElapsed()) {
-			this->phase = 2;
-		}
-	} else if(phase == 2) {
-		scoreTimer.update();
-		if(scoreTimer.isElapsed()) {
-			this->phase = 3;
-		}
-	}
+        if (phase == 0) {
+                yourScoreTimer.update();
+                if (yourScoreTimer.isElapsed()) {
+                        this->phase = 1;
+                }
+        } else if (phase == 1) {
+                isTimer.update();
+                if (isTimer.isElapsed()) {
+                        this->phase = 2;
+                }
+        } else if (phase == 2) {
+                scoreTimer.update();
+                if (scoreTimer.isElapsed()) {
+                        this->phase = 3;
+                }
+        } else if (phase == 3) {
+                returnTimer.update();
+                if (returnTimer.isElapsed()) {
+                        this->phase = 4;
+                }
+        }
+        checkSpaceKey();
+        alphaDuration.update();
 }
 void ResultScene::draw() {
-	auto tex = textureManager->getTexture("./assets/image/TimeOver.png");
-	auto size = tex->getSize();
-	auto wsize = gel::Game::getInstance()->getWindowSize();
-	auto centerPos = (wsize - size) / 2;
-	centerPos.y = 0;
-    gel::drawTexture(centerPos, gel::Color4(1, 1, 1, 1), tex);
-	// draw `your score`
-	if(phase >= 1) {
-		tex = textureManager->getTexture("./assets/image/YourScore.png");
-		size = tex->getSize();
-		centerPos.y = size.y + 20;
-    	gel::drawTexture(centerPos, gel::Color4(1, 1, 1, 1), tex);
-	}
-	if(phase >= 2) {
-		tex = textureManager->getTexture("./assets/image/Is.png");
-		size = tex->getSize();
-		auto centerPos2 = (wsize - size) / 2;
-		centerPos2.y = centerPos.y + size.y;
-		centerPos = centerPos2;
-    	gel::drawTexture(centerPos2, gel::Color4(1, 1, 1, 1), tex);
-	}
-	if(phase >= 3) {
-		size = scoreNum.calcSize(playResult.getCurrentScore());
-		centerPos = (wsize - size) / 2;
-		scoreNum.position = centerPos;
-		scoreNum.draw(playResult.getCurrentScore());
-	}
+        auto tex = textureManager->getTexture("./assets/image/TimeOver.png");
+        auto size = tex->getSize();
+        auto wsize = gel::Game::getInstance()->getWindowSize();
+        auto centerPos = (wsize - size) / 2;
+        centerPos.y = 0;
+        gel::drawTexture(centerPos, gel::Color4(1, 1, 1, 1), tex);
+        // draw `your score`
+        if (phase >= 1) {
+                tex =
+                    textureManager->getTexture("./assets/image/YourScore.png");
+                size = tex->getSize();
+                centerPos.y = size.y + 20;
+                gel::drawTexture(centerPos, gel::Color4(1, 1, 1, 1), tex);
+        }
+        if (phase >= 2) {
+                tex = textureManager->getTexture("./assets/image/Is.png");
+                size = tex->getSize();
+                auto centerPos2 = (wsize - size) / 2;
+                centerPos2.y = centerPos.y + size.y;
+                centerPos = centerPos2;
+                gel::drawTexture(centerPos2, gel::Color4(1, 1, 1, 1), tex);
+        }
+        if (phase >= 3) {
+                size = scoreNum.calcSize(playResult.getCurrentScore());
+                centerPos = (wsize - size) / 2;
+                scoreNum.position = centerPos;
+                scoreNum.draw(playResult.getCurrentScore());
+        }
+        if (phase >= 4) {
+                tex = textureManager->getTexture(
+                    "./assets/image/ReturnToTitleBySpaceKey.png");
+                size = tex->getSize();
+                centerPos = (wsize - size) / 2;
+                centerPos.y =
+                    gel::Game::getInstance()->getWindowHeight() - size.y;
+                gel::drawTexture(centerPos,
+                                 gel::Color4(1, 1, 1, alphaDuration.progress()),
+                                 tex);
+        }
 }
-void ResultScene::hide() {
-	this->finished = false;
-}
+void ResultScene::hide() { this->finished = false; }
 
 bool ResultScene::isFinished() const { return finished; }
 
-std::string ResultScene::getNextScene() const  { return "title";}
+std::string ResultScene::getNextScene() const { return "title"; }
+
+// private
+void ResultScene::checkSpaceKey() {
+        if (this->phase != 4) {
+                return;
+        }
+        if (glfwGetKey(gel::Game::getInstance()->getWindow(), GLFW_KEY_SPACE) ==
+            GLFW_PRESS) {
+                this->transStart = true;
+        }
+        if (!transStart) {
+                return;
+        }
+        transTimer.update();
+        if (transTimer.isElapsed()) {
+                transTimer.reset();
+                this->transStart = false;
+                this->finished = true;
+        }
+}
--- src/game/scene/ResultScene.hpp
+++ src/game/scene/ResultScene.hpp
@@ -1,25 +1,33 @@
 #ifndef GAME_SCENE_RESULTSCENE_HPP
 #define GAME_SCENE_RESULTSCENE_HPP
-#include "../PlayResult.hpp"
-#include "../../gel/gel.hpp"
 #include <memory>
+#include "../../gel/gel.hpp"
+#include "../PlayResult.hpp"
 class ResultScene : public gel::IScene {
-	public:
-	explicit ResultScene(const std::shared_ptr<gel::TextureManager>& textureManager, PlayResult& playResult);
-	void show() override;
-	void update() override;
-	void draw() override;
-	void hide() override;
-	bool isFinished() const override;
-	std::string getNextScene() const override;
-	private:
-	std::shared_ptr<gel::TextureManager> textureManager;
-	gel::Timer yourScoreTimer;
-	gel::Timer isTimer;
-	gel::Timer scoreTimer;
-	gel::Number scoreNum;
-	int phase;
-	PlayResult& playResult;
-	bool finished;
+       public:
+        explicit ResultScene(
+            const std::shared_ptr<gel::TextureManager>& textureManager,
+            PlayResult& playResult);
+        void show() override;
+        void update() override;
+        void draw() override;
+        void hide() override;
+        bool isFinished() const override;
+        std::string getNextScene() const override;
+
+       private:
+        void checkSpaceKey();
+        std::shared_ptr<gel::TextureManager> textureManager;
+        gel::Timer yourScoreTimer;
+        gel::Timer isTimer;
+        gel::Timer scoreTimer;
+        gel::Timer returnTimer;
+        gel::Timer transTimer;
+        gel::Number scoreNum;
+        gel::Duration alphaDuration;
+        int phase;
+        PlayResult& playResult;
+        bool finished;
+        bool transStart;
 };
 #endif
